9733672e6f9f5801c47c97e2b7fcf63161ab8cd6
VadymBoguslavsky <axixe92@gmail.com>
2017-05-23 15:06:15 +0300

    work

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -16,9 +16,9 @@ const CONFIG_PRODUCTION: AppConfig = {
 };
 
 const CONFIG_DEVELOPMENT: AppConfig = {
-  serverUrl: "http://localhost:3000",
-  apiEndpoint: "http://localhost:3000/api",
-  clientId: "bb9818f70964383d26bd57bb60715f7430311b6ccb5021152321932a8f261a43"
+  serverUrl: "https://vadimapi.herokuapp.com",
+  apiEndpoint: "https://vadimapi.herokuapp.com/api",
+  clientId: "77d40037169ced17fa95bb17a8b2a55b0fcfabbf9b37400fa104a01a37fc1c0b"
 };
 
 export function appConfig(): AppConfig {
